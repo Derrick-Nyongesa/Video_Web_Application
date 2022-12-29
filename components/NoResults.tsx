3688349d3700e025ddd5460e7b78274b652a2e0a
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2022-12-29 09:15:11 +0300

    video component

---
+++ components/NoResults.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface IProps {
+  text: String;
+}
+
+function NoResults({ text }: IProps) {
+  return <div>NoResults</div>;
+}
+
+export default NoResults;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,38 @@
 import type { NextPage } from "next";
+import axios from "axios";
+import { Video } from "./types";
+import VideoComponent from "../components/VideoComponent";
+import NoResults from "../components/NoResults";
 
-const Home: NextPage = () => {
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
   return (
     <div>
-      <h1 className="text-3x1 font-bold underline">tiktok clone</h1>
+      <h1 className="flex flex-col gap-10 videos h-full">
+        {videos.length ? (
+          videos.map((video: Video) => (
+            <VideoComponent post={video} key={video._id}></VideoComponent>
+          ))
+        ) : (
+          <NoResults text={"No Videos"}></NoResults>
+        )}
+      </h1>
     </div>
   );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+  // console.log(response.data.name);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
 };
 
 export default Home;
