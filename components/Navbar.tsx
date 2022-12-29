013b6b09559801d341b625240d1cb8be9559ebd9
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2022-12-29 09:48:16 +0300

    Google Auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import GoogleLogin from "react-google-login";
-import { GoogleLogout } from "react-google-login";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
 import Logo from "../utils/tiktik-logo.png";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 
 function Navbar() {
+  const user = false;
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -22,6 +22,17 @@ function Navbar() {
           ></Image>
         </div>
       </Link>
+      <div>Search</div>
+      <div>
+        {user ? (
+          <div>logged in</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => console.log(response)}
+            onError={() => console.log("Error")}
+          ></GoogleLogin>
+        )}
+      </div>
     </div>
   );
 }
